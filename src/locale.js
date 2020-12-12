1dc6343fcca9ba8d3d1bdf78e52beccff83b218e
ttsukagoshi <55706659+ttsukagoshi@users.noreply.github.com>
2020-12-13 07:17:24 +0900

    Fix ESLint errors no-unused-vars, no-undef

--- src/locale.js
+++ src/locale.js
@@ -20,6 +20,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+/* exported LocalizedMessage */
+/* global MESSAGE */
+
 class LocalizedMessage {
   constructor(userLocale = 'en_US') {
     this.locale = userLocale;
